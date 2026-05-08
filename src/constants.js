971a04873983255c75a1ea1a993aa817e40d8cc7
haruki <95395257+haruki7777@users.noreply.github.com>
2026-05-09 02:01:10 +0900

    Add ticket constants

---
+++ src/constants.js
@@ -0,0 +1,35 @@
+export const TICKET_CATEGORIES = [
+  {
+    key: 'support',
+    label: '일반 문의',
+    description: '궁금한 점이나 도움이 필요할 때',
+    emoji: '❄️',
+  },
+  {
+    key: 'report',
+    label: '신고 문의',
+    description: '유저 신고, 서버 문제 제보',
+    emoji: '🚨',
+  },
+  {
+    key: 'payment',
+    label: '결제/후원 문의',
+    description: '결제, 후원, 보상 관련 문의',
+    emoji: '💎',
+  },
+  {
+    key: 'partner',
+    label: '제휴 문의',
+    description: '서버 제휴 및 협업 문의',
+    emoji: '🤝',
+  },
+];
+
+export const customIds = {
+  ticketSelect: 'yukiha_ticket_select',
+  ticketModalPrefix: 'yukiha_ticket_modal:',
+  ticketReasonInput: 'yukiha_ticket_reason',
+  closeTicket: 'yukiha_ticket_close',
+  reopenTicket: 'yukiha_ticket_reopen',
+  deleteTicket: 'yukiha_ticket_delete',
+};
