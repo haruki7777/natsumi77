823ef18cf62df86536d7a4b4d21a43a42516062e
haruki <95395257+haruki7777@users.noreply.github.com>
2026-05-09 02:02:52 +0900

    Add command registry

---
+++ src/commands/index.js
@@ -0,0 +1,4 @@
+import { ticketSetupCommand } from './ticketSetup.js';
+import { ticketManageCommand } from './ticketManage.js';
+
+export const commands = [ticketSetupCommand, ticketManageCommand];
