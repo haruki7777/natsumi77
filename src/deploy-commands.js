4462641e9c3956430dfe9caa15f9b16d412dfa47
haruki <95395257+haruki7777@users.noreply.github.com>
2026-05-09 04:02:26 +0900

    Register slash commands globally

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -7,8 +7,9 @@ validateConfig();
 const rest = new REST({ version: '10' }).setToken(config.token);
 const body = commands.map((command) => command.data.toJSON());
 
-console.log(`[DEPLOY] ${body.length}개 명령어 등록 중...`);
+console.log(`[DEPLOY] ${body.length}개 전역 명령어 등록 중...`);
 
-await rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body });
+await rest.put(Routes.applicationCommands(config.clientId), { body });
 
-console.log('[DEPLOY] 서버 전용 슬래시 명령어 등록 완료');
+console.log('[DEPLOY] 전역 슬래시 명령어 등록 완료');
+console.log('[DEPLOY] 전역 명령어는 디스코드 반영까지 몇 분 걸릴 수 있습니다.');
