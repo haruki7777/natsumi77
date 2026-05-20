a94e8be2a6806100dec4d1de978b03fad3abe08a
haruki <95395257+haruki7777@users.noreply.github.com>
2026-05-21 05:30:37 +0900

    Register utility welcome and moderation commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,11 +2,25 @@ import { ticketSetupCommand } from './ticketSetup.js';
 import { ticketManageCommand } from './ticketManage.js';
 import { ticketSetupEnglishCommand } from './ticketSetupEnglish.js';
 import { ticketManageEnglishCommand } from './ticketManageEnglish.js';
+import { pingCommand } from './ping.js';
+import { helpCommand } from './help.js';
+import { welcomeSetupCommand } from './welcomeSetup.js';
+import { welcomeTestCommand } from './welcomeTest.js';
+import { adminSetupCommand } from './adminSetup.js';
+import { kickCommand } from './kick.js';
+import { banCommand } from './ban.js';
 
 const list = [];
 list.push(ticketSetupCommand);
 list.push(ticketManageCommand);
 list.push(ticketSetupEnglishCommand);
 list.push(ticketManageEnglishCommand);
+list.push(pingCommand);
+list.push(helpCommand);
+list.push(welcomeSetupCommand);
+list.push(welcomeTestCommand);
+list.push(adminSetupCommand);
+list.push(kickCommand);
+list.push(banCommand);
 
 export const commands = list;
