416ebd7664a275e0fdb98fc0a9be47e814808ac8
haruki <95395257+haruki7777@users.noreply.github.com>
2026-05-21 05:31:05 +0900

    Connect welcome and goodbye member events

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,13 +1,23 @@
-import { ActivityType } from 'discord.js';
+import { ActivityType, Events } from 'discord.js';
 import { commands } from '../commands/index.js';
 import { registerGuildCommands } from '../utils/registerCommands.js';
+import { handleGuildMemberAdd, handleGuildMemberRemove } from './memberEvents.js';
+
+let memberEventsRegistered = false;
 
 export async function handleReady(client) {
   console.log(`[BOT] Logged in as ${client.user.tag}`);
   client.user.setPresence({
-    activities: [{ name: '티켓 문의를 감시하는 중 ❄️', type: ActivityType.Watching }],
+    activities: [{ name: '유키하 서버 관리 시스템 ❄️', type: ActivityType.Watching }],
     status: 'online',
   });
 
   await registerGuildCommands(client, commands);
+
+  if (!memberEventsRegistered) {
+    client.on(Events.GuildMemberAdd, handleGuildMemberAdd);
+    client.on(Events.GuildMemberRemove, handleGuildMemberRemove);
+    memberEventsRegistered = true;
+    console.log('[EVENTS] 환영/퇴장 이벤트 연결 완료');
+  }
 }
