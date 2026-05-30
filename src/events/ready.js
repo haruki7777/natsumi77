2ca8afdff23ac7e7047477ed3b427fc6a3b99689
haruki <95395257+haruki7777@users.noreply.github.com>
2026-05-31 08:53:50 +0900

    Run monitor on ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,7 @@
 import { ActivityType, Events } from 'discord.js';
 import { commands } from '../commands/index.js';
 import { registerGuildCommands } from '../utils/registerCommands.js';
+import { startBotMonitor } from '../utils/botMonitor.js';
 import { handleGuildMemberAdd, handleGuildMemberRemove } from './memberEvents.js';
 
 let memberEventsRegistered = false;
@@ -8,16 +9,17 @@ let memberEventsRegistered = false;
 export async function handleReady(client) {
   console.log(`[BOT] Logged in as ${client.user.tag}`);
   client.user.setPresence({
-    activities: [{ name: '유키하 서버 관리 시스템 ❄️', type: ActivityType.Watching }],
+    activities: [{ name: 'YUKIHA server manager', type: ActivityType.Watching }],
     status: 'online',
   });
 
   await registerGuildCommands(client, commands);
+  startBotMonitor(client);
 
   if (!memberEventsRegistered) {
     client.on(Events.GuildMemberAdd, handleGuildMemberAdd);
     client.on(Events.GuildMemberRemove, handleGuildMemberRemove);
     memberEventsRegistered = true;
-    console.log('[EVENTS] 환영/퇴장 이벤트 연결 완료');
+    console.log('[EVENTS] member events registered');
   }
 }
